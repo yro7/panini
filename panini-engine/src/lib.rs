79dcf6942d4ab4e7087eecb1a1ec758d4459ebf9
yro7 <yronusa2000@gmail.com>
2026-04-10 21:22:41 +0200

    refactor: remove legacy FeatureExtractionResponse and associated schema generation logic

--- panini-engine/src/lib.rs
+++ panini-engine/src/lib.rs
@@ -4,7 +4,7 @@ pub mod llm_utils;
 pub mod prompts;
 
 pub use extractor::{
-    extract_features_via_llm, extract_with_components, ExtractionError, ExtractionOptions,
+    extract_with_components, ExtractionError, ExtractionOptions,
     ExtractionParseError, PreviousAttempt,
 };
 pub use prompts::ExtractionRequest;
