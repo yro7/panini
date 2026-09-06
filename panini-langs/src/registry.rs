30a0218f1836f5d1ccdde80c5688a196490086eb
yro7 <yronusa2000@gmail.com>
2026-09-06 21:47:32 +0200

    feat: add swahili

--- panini-langs/src/registry.rs
+++ panini-langs/src/registry.rs
@@ -218,7 +218,7 @@ with_languages!(generate_registry);
 
 #[cfg(test)]
 mod tests {
-    use crate::{Basque, Danish, Indonesian, Korean, Polish, Turkish};
+    use crate::{Basque, Danish, Indonesian, Korean, Polish, Swahili, Turkish};
     use panini_core::component::AnalysisComponent;
     use panini_core::components::*;
     use panini_core::morpheme::Agglutinative;
@@ -235,6 +235,7 @@ mod tests {
         assert_agglutinative_inventory_valid::<Indonesian>();
         assert_agglutinative_inventory_valid::<Korean>();
         assert_agglutinative_inventory_valid::<Basque>();
+        assert_agglutinative_inventory_valid::<Swahili>();
     }
 
     #[test]
